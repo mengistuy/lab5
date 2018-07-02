0110bdd71da63193dc1c547aa37ff705853cd138
Mengistu Bogale <mengistuy@gmail.com>
2018-07-01 19:25:19 -0500

    promise

--- expApp.js
+++ expApp.js
@@ -1,39 +1,54 @@
+const axois=require('axios')
+const Rx=require('rxjs')
 const express=require('express')
 const app=express()
-const axois=require('axios')
-const users={
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-  }
-axois.get('http://jsonplaceholder.typicode.com/users/')
-    .then((users) => {
-        app.get('/users',(req,res)=>{
-            res.send(users);
-        });
-}).catch((err) => {
-    
-});
+//promise
+function pro(){
+  axois.get('http://jsonplaceholder.typicode.com/users/')
+  .then((users) => {
+    app.get('/promise/users',(req,res)=>{
+    res.send(users.data)
+    });
+  }).catch((err) => {
+    res.send(err)
+  });
+}
+pro();
+// app.get('/promise/users',(req,res)=>{
+//  // Rx.Observable.of(axois.get('http://jsonplaceholder.typicode.com/users/'))
+//   //.toPromise()
+//   //new Promise(axois.get('http://jsonplaceholder.typicode.com/users/'))
+//   pro()
+//   .then((users) => {
+//     res.send(users)
+//   }).catch((err) => {
+//     res.send(err)
+//   });
+// })
 
-// app.get('http://jsonplaceholder.typicode.com/users',(req,res)=>{
-//     res.send(users);
-// });
+const usingAsync=async ()=>{
+              try{
+                const users=await axois.get('http://jsonplaceholder.typicode.com/users/')
+                app.get('/await/users',(req,res)=>{
+                  res.send(users.data);
+                });
+              }
+              catch(err){
+                res.send(err)
+              }                 
+  }
 app.listen(8080);
+usingAsync();
+
+// const usingAsync=async ()=>{
+//   await axois.get('http://jsonplaceholder.typicode.com/users/')
+//       .then((users) => {
+//           app.get('/await/users',(req,res)=>{
+//               res.send(users.data);
+//           });
+//   }).catch((err) => {
+//      res.send(err)
+//   });
+//   }
+// app.listen(8080);
+// usingAsync();
