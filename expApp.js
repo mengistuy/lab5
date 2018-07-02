7fd6471f4e77cf155b269e2bc89dc5e538335408
Mengistu Bogale <mengistuy@gmail.com>
2018-07-01 21:03:26 -0500

    x-powered-by

--- expApp.js
+++ expApp.js
@@ -1,7 +1,9 @@
 const axois=require('axios')
 const Rx=require('rxjs')
 const express=require('express')
-const app=express()
+const app=express();
+app.enable('case sensitive routing');
+app.disable('x-powered-by');
 //promise
 function pro(){
   axois.get('http://jsonplaceholder.typicode.com/users/')
@@ -14,18 +16,6 @@ function pro(){
   });
 }
 pro();
-// app.get('/promise/users',(req,res)=>{
-//  // Rx.Observable.of(axois.get('http://jsonplaceholder.typicode.com/users/'))
-//   //.toPromise()
-//   //new Promise(axois.get('http://jsonplaceholder.typicode.com/users/'))
-//   pro()
-//   .then((users) => {
-//     res.send(users)
-//   }).catch((err) => {
-//     res.send(err)
-//   });
-// })
-
 const usingAsync=async ()=>{
               try{
                 const users=await axois.get('http://jsonplaceholder.typicode.com/users/')
@@ -40,15 +30,14 @@ const usingAsync=async ()=>{
 app.listen(8080);
 usingAsync();
 
-// const usingAsync=async ()=>{
-//   await axois.get('http://jsonplaceholder.typicode.com/users/')
-//       .then((users) => {
-//           app.get('/await/users',(req,res)=>{
-//               res.send(users.data);
-//           });
+// app.get('/observable/users',(req,res)=>{
+//  // Rx.Observable.of(axois.get('http://jsonplaceholder.typicode.com/users/'))
+//   //.toPromise()
+//   //new Promise(axois.get('http://jsonplaceholder.typicode.com/users/'))
+//   pro()
+//   .then((users) => {
+//     res.send(users)
 //   }).catch((err) => {
-//      res.send(err)
+//     res.send(err)
 //   });
-//   }
-// app.listen(8080);
-// usingAsync();
+// })
